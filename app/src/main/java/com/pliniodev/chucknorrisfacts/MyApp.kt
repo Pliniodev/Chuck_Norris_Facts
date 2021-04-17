f47ca8e9cdcdecc2117e241c34b6bc85f7754bde
Pliniodev <plinioprojetos@hotmail.com>
2021-04-17 11:29:48 -0300

    Adding modules network, api and repository and change androidLogger

--- app/src/main/java/com/pliniodev/chucknorrisfacts/MyApp.kt
+++ app/src/main/java/com/pliniodev/chucknorrisfacts/MyApp.kt
@@ -1,11 +1,11 @@
 package com.pliniodev.chucknorrisfacts
 
 import android.app.Application
-import com.pliniodev.chucknorrisfacts.di.mainModule
-import com.pliniodev.chucknorrisfacts.di.searchModule
+import com.pliniodev.chucknorrisfacts.di.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class MyApp: Application() {
 
@@ -13,12 +13,21 @@ class MyApp: Application() {
         super.onCreate()
 
         startKoin {
-            androidLogger()
+            /**
+             * (Level.NONE) foi usado devido a este bug na versão 2.0.0 do koin
+             * --> No virtual method elapsedNow()D in class Lkotlin/time/TimeMark...
+             * ISSUE relacionada https://github.com/InsertKoinIO/koin/issues/847
+             * parece ter sido resolvido na versão 2.2.0-alpha-1
+             */
+            androidLogger(Level.NONE)
             androidContext(this@MyApp)
 
             modules(
                 mainModule,
-                searchModule
+                searchModule,
+                networkModule,
+                repositoryModule,
+                apiModule
             )
         }
     }
