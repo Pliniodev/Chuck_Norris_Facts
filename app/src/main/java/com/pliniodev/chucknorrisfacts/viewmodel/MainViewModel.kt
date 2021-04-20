91e6d6e492923db725275157af5ad1cb33f57d78
Pliniodev <plinioprojetos@hotmail.com>
2021-04-20 19:52:12 -0300

    Updating some minimum values on strings and layout of card facts

--- app/src/main/java/com/pliniodev/chucknorrisfacts/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/pliniodev/chucknorrisfacts/viewmodel/MainViewModel.kt
@@ -55,8 +55,9 @@ class MainViewModel(
     }
 
     companion object {
-        private const val VIEW_FLIPPER_FACTS = 1
-        private const val VIEW_FLIPPER_ERROR = 2
+        private const val VIEW_FLIPPER_FACTS = 2
+        private const val VIEW_FLIPPER_ERROR = 3
+
     }
 
 }
