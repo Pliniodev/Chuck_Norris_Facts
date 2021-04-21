460bedf1918858b9ecba1110b0003bc9e4dcaab4
Pliniodev <plinioprojetos@hotmail.com>
2021-04-21 20:10:10 -0300

    Adding constants object

---
+++ app/src/main/java/com/pliniodev/chucknorrisfacts/constants/Constants.kt
@@ -0,0 +1,10 @@
+package com.pliniodev.chucknorrisfacts.constants
+
+
+object Constants {
+
+    const val SEARCH_MESSAGE = "searchMessage"
+    const val IS_SEARCH_BY_RANDOM = "isSearchByRandom"
+    const val IS_SEARCH_BY_CATEGORY = "isSearchByCategory"
+
+}
