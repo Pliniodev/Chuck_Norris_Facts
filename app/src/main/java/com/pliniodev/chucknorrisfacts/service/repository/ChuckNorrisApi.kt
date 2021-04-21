56c8c9278b44fe793289de0b071f1ce400676ea0
Pliniodev <plinioprojetos@hotmail.com>
2021-04-21 20:11:52 -0300

    Updating repository and queries

--- app/src/main/java/com/pliniodev/chucknorrisfacts/service/repository/ChuckNorrisApi.kt
+++ app/src/main/java/com/pliniodev/chucknorrisfacts/service/repository/ChuckNorrisApi.kt
@@ -1,16 +1,19 @@
 package com.pliniodev.chucknorrisfacts.service.repository
 
 import com.pliniodev.chucknorrisfacts.data.response.FactBodyResponse
+import com.pliniodev.chucknorrisfacts.data.response.FactDetailsResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ChuckNorrisApi {
     @GET("/jokes/search")
-    suspend fun getFreeQuery(
-        @Query(
-            value = "query",
-            encoded = true
-        ) query: String
-    ): Response<FactBodyResponse>
+    suspend fun getByFreeQuery(@Query(value = "query", encoded = true) query: String ): Response<FactBodyResponse>
+
+    @GET("/jokes/random")
+    suspend fun getByCategory(@Query(value = "category", encoded = true) category: String ): Response<FactDetailsResponse>
+
+    @GET("/jokes/random")
+    suspend fun getRandom(): Response<FactDetailsResponse>
 }
--- app/src/main/java/com/pliniodev/chucknorrisfacts/service/repository/ChuckNorrisRepositoryImpl.kt
+++ app/src/main/java/com/pliniodev/chucknorrisfacts/service/repository/ChuckNorrisRepositoryImpl.kt
@@ -2,42 +2,100 @@ package com.pliniodev.chucknorrisfacts.service.repository
 
 import android.content.Context
 import com.pliniodev.chucknorrisfacts.service.model.Fact
-import com.pliniodev.chucknorrisfacts.service.retrofit.RetrofitClient
 import com.pliniodev.chucknorrisfacts.service.utils.CheckNetworkConnection.isOnline
 import com.pliniodev.chucknorrisfacts.service.utils.CheckNetworkConnection.noNetworkConnectivityError
 import com.pliniodev.chucknorrisfacts.service.utils.FactsResult
 
 class ChuckNorrisRepositoryImpl(
-    private val retrofitClient: RetrofitClient,
     private val context: Context,
+    private val api: ChuckNorrisApi
 ) : ChuckNorrisRepository {
 
-    override suspend fun getFact(query: String, resultCallback: (result: FactsResult) -> Unit) {
-
+    override suspend fun getByFreeQuery(
+        query: String,
+        factsResult: (result: FactsResult) -> Unit
+    ) {
         if (isOnline(context)) {
-            val response = retrofitClient.retrofitService.getFreeQuery(query)
+
+            val responseByFreeQuery = api.getByFreeQuery(query)
 
             try {
                 val factDetailsResponse: MutableList<Fact> = mutableListOf()
-                if (response.isSuccessful) {
+                if (responseByFreeQuery.isSuccessful) {
 
-                    response.body()?.let { factsResultResponse ->
+                    responseByFreeQuery.body()?.let { factsResultResponse ->
                         for (result in factsResultResponse.result) {
                             val fact = result.getFactModel()
                             factDetailsResponse.add(fact)
                         }
                     }
-                    resultCallback(FactsResult.Success(factDetailsResponse))
+                    factsResult(FactsResult.Success(factDetailsResponse))
 
                 } else {
-                    resultCallback(FactsResult.ApiError(response.code()))
+                    factsResult(FactsResult.ApiError(responseByFreeQuery.code()))
                 }
 
             } catch (e: Exception) {
-                resultCallback(FactsResult.ServerError)
+                factsResult(FactsResult.ServerError)
             }
         } else {
-            resultCallback(noNetworkConnectivityError())
+            factsResult(noNetworkConnectivityError())
+        }
+    }
+
+    override suspend fun getByCategory(
+        category: String,
+        factsResult: (result: FactsResult) -> Unit
+    ) {
+        if (isOnline(context)) {
+
+            val responseByCategory = api.getByCategory(category)
+
+            try {
+                val factDetailsResponse: MutableList<Fact> = mutableListOf()
+                if (responseByCategory.isSuccessful) {
+
+                    responseByCategory.body()?.let { factsResultResponse ->
+
+                        factDetailsResponse.add(factsResultResponse.getFactModel())
+
+                    }
+                    factsResult(FactsResult.Success(factDetailsResponse))
+
+                } else {
+                    factsResult(FactsResult.ApiError(responseByCategory.code()))
+                }
+
+            } catch (e: Exception) {
+                factsResult(FactsResult.ServerError)
+            }
+        } else {
+            factsResult(noNetworkConnectivityError())
+        }
+    }
+
+    override suspend fun getByRandom(
+        factsResult: (result: FactsResult) -> Unit
+    ) {
+        val responseByRandom = api.getRandom()
+
+        if (isOnline(context)) {
+            try {
+                val factDetailsResponse: MutableList<Fact> = mutableListOf()
+                if (responseByRandom.isSuccessful) {
+
+                    responseByRandom.body()?.let { factsResultResponse ->
+
+                        factDetailsResponse.add(factsResultResponse.getFactModel())
+
+                    }
+                    factsResult(FactsResult.Success(factDetailsResponse))
+                }
+            } catch (t: Throwable) {
+                factsResult(FactsResult.ApiError(responseByRandom.code()))
+            }
+        } else {
+            factsResult(noNetworkConnectivityError())
         }
     }
 }
