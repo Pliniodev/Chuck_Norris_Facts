3f72a621e5c1c646c31af08c045fd8a701d4e09c
Pliniodev <plinioprojetos@hotmail.com>
2021-04-23 01:01:02 -0300

    Changing list of categories to viewModel

--- app/src/main/java/com/pliniodev/chucknorrisfacts/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/pliniodev/chucknorrisfacts/viewmodel/SearchViewModel.kt
@@ -1,5 +1,6 @@
 package com.pliniodev.chucknorrisfacts.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.pliniodev.chucknorrisfacts.R
@@ -9,7 +10,31 @@ class SearchViewModel: ViewModel() {
 
     val validatorMsgLiveData: MutableLiveData<Pair<Boolean, Int?>> = MutableLiveData()
 
+    val listCategoryLiveData: MutableLiveData<List<String>> = MutableLiveData()
+
     fun validate(search: String) {
         validatorMsgLiveData.value = validateSearchText(search)
+    }
+
+    fun getListCategories() {
+        val items = listOf(
+            "Animal",
+            "Career",
+            "Celebrity",
+            "Dev",
+            "Explicit",
+            "Fashion",
+            "Food",
+            "History",
+            "Money",
+            "Movie",
+            "Music",
+            "Political",
+            "Religion",
+            "Science",
+            "Sport",
+            "Travel"
+        )//TEMPORÁRIO
+        listCategoryLiveData.value = items
     }
 }
