4e8f77f33c13651bb498d25a488afa0a6f902fe3
Pliniodev <plinioprojetos@hotmail.com>
2021-04-23 17:53:24 -0300

    Updating functions on repository

--- app/src/main/java/com/pliniodev/chucknorrisfacts/service/repository/ChuckNorrisRepository.kt
+++ app/src/main/java/com/pliniodev/chucknorrisfacts/service/repository/ChuckNorrisRepository.kt
@@ -1,11 +1,16 @@
 package com.pliniodev.chucknorrisfacts.service.repository
 
+import com.pliniodev.chucknorrisfacts.data.response.FactBodyResponse
+import com.pliniodev.chucknorrisfacts.data.response.FactDetailsResponse
+import com.pliniodev.chucknorrisfacts.service.model.Fact
 import com.pliniodev.chucknorrisfacts.service.utils.FactsResult
 
 interface ChuckNorrisRepository {
-    suspend fun getByFreeQuery(query: String, factsResult: (result: FactsResult) -> Unit)
+    suspend fun getByFreeQuery(query: String, factsResult: (result: FactsResult<List<Fact>>) -> Unit)
 
-    suspend fun getByCategory(category: String, factsResult: (result: FactsResult) -> Unit)
+    suspend fun getByCategory(category: String, factsResult: (result: FactsResult<List<Fact>>) -> Unit)
 
-    suspend fun getByRandom(factsResult: (result: FactsResult) -> Unit)
+    suspend fun getByRandom(factsResult: (result: FactsResult<List<Fact>>) -> Unit)
+
+    suspend fun getCategoriesList(factsResult: (result: FactsResult<List<String>>) -> Unit)
 }
