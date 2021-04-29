cc7bae0a6219a75a5b8cdae75bca08f42586aaa0
Pliniodev <plinioprojetos@hotmail.com>
2021-04-29 11:32:44 -0300

    Adding tests to SafeApiCallTest

---
+++ app/src/test/java/com/pliniodev/chucknorrisfacts/service/utils/SafeApiCallTest.kt
@@ -0,0 +1,38 @@
+package com.pliniodev.chucknorrisfacts.service.utils
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import java.io.IOException
+
+class SafeApiCallTest{
+
+    @ExperimentalCoroutinesApi
+    private val dispatcher = TestCoroutineDispatcher()
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `should emit the result as success when lambda returns successfully`() {
+        runBlockingTest {
+            val lambdaResult = true
+            val result = safeApiCall(dispatcher) { lambdaResult }
+            assertEquals(FactsResult.Success(lambdaResult),result)
+        }
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `should emit the result as connectionError when lambda returns throws IOException`() {
+        runBlockingTest {
+            val result = safeApiCall(dispatcher) { throw IOException() }
+            assertEquals(FactsResult.ConnectionError, result)
+        }
+    }
+}
