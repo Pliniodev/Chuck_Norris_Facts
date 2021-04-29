0d999560c21b3e3d86ced92b0452e2121c396e3a
Pliniodev <plinioprojetos@hotmail.com>
2021-04-29 11:23:37 -0300

    Correcting params of funs

--- app/src/main/java/com/pliniodev/chucknorrisfacts/service/repository/ChuckNorrisRepository.kt
+++ app/src/main/java/com/pliniodev/chucknorrisfacts/service/repository/ChuckNorrisRepository.kt
@@ -1,16 +1,24 @@
 package com.pliniodev.chucknorrisfacts.service.repository
 
-import com.pliniodev.chucknorrisfacts.data.response.FactBodyResponse
-import com.pliniodev.chucknorrisfacts.data.response.FactDetailsResponse
 import com.pliniodev.chucknorrisfacts.service.model.Fact
 import com.pliniodev.chucknorrisfacts.service.utils.FactsResult
 
 interface ChuckNorrisRepository {
-    suspend fun getByFreeQuery(query: String, factsResult: (result: FactsResult<List<Fact>>) -> Unit)
 
-    suspend fun getByCategory(category: String, factsResult: (result: FactsResult<List<Fact>>) -> Unit)
+    suspend fun getByFreeQuery(query: String): FactsResult<List<Fact>>
 
-    suspend fun getByRandom(factsResult: (result: FactsResult<List<Fact>>) -> Unit)
+    suspend fun getByCategory(category: String): FactsResult<List<Fact>>
 
-    suspend fun getCategoriesList(factsResult: (result: FactsResult<List<String>>) -> Unit)
+    suspend fun getByRandom(): FactsResult<List<Fact>>
+
+    suspend fun getCategoriesList(): FactsResult<List<String>>
+
+
+//    suspend fun getByFreeQuery(query: String, factsResult: (result: FactsResult<List<Fact>>) -> Unit)
+//
+//    suspend fun getByCategory(category: String, factsResult: (result: FactsResult<List<Fact>>) -> Unit)
+//
+//    suspend fun getByRandom(factsResult: (result: FactsResult<List<Fact>>) -> Unit)
+//
+//    suspend fun getCategoriesList(factsResult: (result: FactsResult<List<String>>) -> Unit)
 }
